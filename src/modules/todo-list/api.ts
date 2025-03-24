5e57b40e2c6766b76b3f13e65a03b902de31d61c
Aa <aa@example.com>
2025-03-24 16:32:57 +0200

    updated data

--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -1,5 +1,15 @@
 const BASE_URL = "http://localhost:3011";
 
+export type PaginatedResult<T> = {
+  data: T[];
+  first: number;
+  items: number;
+  last: number | null;
+  next: number | null;
+  pages: number;
+  prev: number | null;
+};
+
 export type TodoDto = {
   id: number;
   text: string;
@@ -7,9 +17,11 @@ export type TodoDto = {
 };
 
 export const todoListApi = {
-  getTodoList: ({ signal }: { signal: AbortSignal }) => {
-    return fetch(`${BASE_URL}/tasks`, { signal }).then(
-      (res) => res.json() as Promise<TodoDto[]>,
-    );
+  getTodoList: async (
+    { page }: { page: number },
+    { signal }: { signal: AbortSignal },
+  ) => {
+    const res = await fetch(`${BASE_URL}/tasks?_page=${page}`, { signal });
+    return await (res.json() as Promise<PaginatedResult<TodoDto>>);
   },
 };
--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,10 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api.ts";
+import { useState } from "react";
 
 export function TodoList() {
-  const { data, error, isPending } = useQuery({
-    queryKey: ["tasks", "list"],
-    queryFn: todoListApi.getTodoList,
+  const [page, setPage] = useState(1);
+  const {
+    data: todoItems,
+    error,
+    isPending,
+  } = useQuery({
+    queryKey: ["tasks", "list", { page }],
+    queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
   });
 
   if (isPending) {
@@ -18,11 +24,25 @@ export function TodoList() {
     <div className="p-5 mx-auto max-w-[1200px] mt-4">
       <h1 className="text-3xl font-bold underline mb-1">todo list</h1>
       <div className="flex flex-col gap-3">
-        {data.map((todo) => (
+        {todoItems.data.map((todo) => (
           <div className="border border-slate-500 rounded-xl p-3" key={todo.id}>
             {todo.text}
           </div>
         ))}
+      </div>
+      <div className="mt-3   flex gap-3">
+        <button
+          onClick={() => setPage((p) => Math.max(p - 1, 1))}
+          className="p-3 rounded border border-teal-500 cursor-pointer"
+        >
+          prev
+        </button>
+        <button
+          onClick={() => setPage((p) => Math.min(p + 1, todoItems?.pages))}
+          className="p-3 rounded border border-teal-500 cursor-pointer"
+        >
+          next
+        </button>
       </div>
     </div>
   );
