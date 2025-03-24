52ceb860a7d131aebb3c697a0a07078786cf13de
Aa <aa@example.com>
2025-03-24 14:02:58 +0200

    added basic

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,3 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+
+type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+export const getTasks = () => {
+  return new Promise<Todo[]>((res) => {
+    setTimeout(() => {
+      res([
+        {
+          id: 1,
+          text: "Tanstack Query 101",
+          done: false,
+        },
+        {
+          id: 2,
+          text: "Tanstack Query 201",
+          done: false,
+        },
+        {
+          id: 3,
+          text: "Tanstack Query 202",
+          done: false,
+        },
+        {
+          id: 4,
+          text: "Tanstack Query 203",
+          done: false,
+        },
+        {
+          id: 5,
+          text: "Tanstack Query 204",
+          done: false,
+        },
+      ]);
+    }, 1000);
+  });
+};
+
 export function TodoList() {
-  return <div>todo list</div>;
+  const { data, error, isPending } = useQuery({
+    queryKey: ["tasks", "list"],
+    queryFn: getTasks,
+  });
+
+  if (isPending) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  return (
+    <div>
+      <p>todo list</p>
+      {data.map((todo) => (
+        <div key={todo.id}>{todo.text}</div>
+      ))}
+    </div>
+  );
 }
