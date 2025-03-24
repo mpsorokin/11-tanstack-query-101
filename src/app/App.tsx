9e244638140b9a4f1380ff2d98216d1c405cd233
Aa <aa@example.com>
2025-03-24 13:36:56 +0200

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/query-client.ts";
+import { TodoList } from "../modules/todo-list/todo-list.tsx";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>hello</h1>
+      <TodoList />
     </QueryClientProvider>
   );
 }
