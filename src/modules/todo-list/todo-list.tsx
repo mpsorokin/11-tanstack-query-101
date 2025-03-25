a7e1804e540d3e9fdd1f67a1976fea486bb47d8e
Aa <aa@example.com>
2025-03-25 14:36:14 +0200

    added infinity scroll

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,10 +1,14 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
 import { todoListApi } from "./api.ts";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export function TodoList() {
-  const [page, setPage] = useState(1);
-  const {
+  //const [page, setPage] = useState(1);
+  /*const {
     data: todoItems,
     error,
     isPending,
@@ -12,6 +16,24 @@ export function TodoList() {
     queryKey: ["tasks", "list", { page }],
     queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
     placeholderData: keepPreviousData,
+  });*/
+  const {
+    data: todoItems,
+    error,
+    isPending,
+    fetchNextPage,
+    //hasNextPage,
+    //isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["tasks", "list"],
+    queryFn: (meta) => todoListApi.getTodoList({ page: meta.pageParam }, meta),
+    initialPageParam: 1,
+    getNextPageParam: (result) => result.next,
+    select: (result) => result.pages.flatMap((page) => page.data),
+  });
+
+  const cursorRef = useIntersection(() => {
+    fetchNextPage();
   });
 
   if (isPending) {
@@ -21,18 +43,19 @@ export function TodoList() {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+
   return (
     <div className="p-5 mx-auto max-w-[1200px] mt-4">
       <h1 className="text-3xl font-bold underline mb-1">todo list</h1>
       <div className="flex flex-col gap-3">
-        {todoItems.data.map((todo) => (
+        {todoItems.map((todo) => (
           <div className="border border-slate-500 rounded-xl p-3" key={todo.id}>
             {todo.text}
           </div>
         ))}
       </div>
-      <div className="mt-3   flex gap-3">
-        <button
+      <div className="mt-3 flex gap-3" ref={cursorRef}>
+        {/*<button
           onClick={() => setPage((p) => Math.max(p - 1, 1))}
           className="p-3 rounded border border-teal-500 cursor-pointer"
         >
@@ -43,8 +66,29 @@ export function TodoList() {
           className="p-3 rounded border border-teal-500 cursor-pointer"
         >
           next
-        </button>
+        </button>*/}
       </div>
     </div>
   );
+}
+
+export function useIntersection(onIntersect: () => void) {
+  const unsubscribe = useRef(() => {});
+
+  return useCallback((el: HTMLDivElement | null) => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          onIntersect();
+        }
+      });
+    });
+
+    if (el) {
+      observer.observe(el);
+      unsubscribe.current = () => observer.disconnect();
+    } else {
+      unsubscribe.current();
+    }
+  }, []);
 }
