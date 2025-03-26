a364c72b57316d92f20bedaeac6690db8d0414ca
Aa <aa@example.com>
2025-03-26 13:45:57 +0200

    added user api

---
+++ src/modules/auth/api.ts
@@ -0,0 +1,28 @@
+import { queryOptions } from "@tanstack/react-query";
+import { jsonApiInstance } from "../../shared/api/api-instance.ts";
+
+export type UserDto = {
+  id: string;
+  login: string;
+  password: string;
+};
+
+export const authApi = {
+  baseKey: "users",
+
+  getUserById: (id: string) => {
+    return queryOptions({
+      queryKey: [authApi.baseKey, "byId", id],
+      queryFn: (meta) =>
+        jsonApiInstance<UserDto>(`/users/${id}`, {
+          signal: meta.signal,
+        }),
+    });
+  },
+
+  loginUser: (login: string, password: string) => {
+    return jsonApiInstance<UserDto[]>(
+      `/users/?login=${login}&password=${password}`,
+    ).then((res) => res[0] as UserDto | null);
+  },
+};
