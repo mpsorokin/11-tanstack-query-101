1331c9707e12fdf4392ab7a26d1757f727ea3d46
Aa <aa@example.com>
2025-03-26 13:23:14 +0200

    added redux and updated db.json

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "../shared/api/query-client.ts";
 import { TodoList } from "../modules/todo-list/todo-list.tsx";
+import { Provider } from "react-redux";
+import { store } from "../shared/redux.ts";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TodoList />
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
